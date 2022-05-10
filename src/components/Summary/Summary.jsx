e2a6b25e9ebe23d8e628127e63d794aad69f3afa
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-05-10 18:35:23 +0300

    icons added to summary component

--- src/components/Summary/Summary.jsx
+++ src/components/Summary/Summary.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { _calculateAge } from "../../util/ageCalculator";
 import "./Summary.css";
+import { FaPhone, FaUser } from "react-icons/fa";
+import { MdMail, MdLocationPin } from "react-icons/md";
+
 const Summary = (props) => {
   const {
     first_name,
@@ -27,18 +30,31 @@ const Summary = (props) => {
               {_calculateAge(new Date(birth_date))}
             </span>
           </span>
-
           <span className="summary__job">{job}</span>
-          <span className="summary__phone">☎ {phone_number}</span>
-          <span className="summary__email"> ✉ {email}</span>
-
-          <span className="summary__country">
-            <strong>{city}</strong>
-            {",  "}
-            {country.toUpperCase()}
-          </span>
+          <br />
+          <div className="summary__row">
+            <FaPhone size={25} />
+            <span className="summary__phone">{phone_number}</span>
+          </div>
+          <div className="summary__row">
+            <MdMail size={25} />
+            <span className="summary__email">{email}</span>
+          </div>
+     
+          <div className="summary__row">
+            <MdLocationPin size={25} />
+            <span className="summary__country">
+              <strong>{city}</strong>
+              {",  "}
+              {country.toUpperCase()}
+            </span>
+          </div>
         </div>
-        <img src={imageUrl} className="summary__img" />
+        {imageUrl !== "" ? (
+          <img src={imageUrl} className="summary__img" />
+        ) : (
+          <FaUser size={150} />
+        )}
       </div>
     </div>
   );
