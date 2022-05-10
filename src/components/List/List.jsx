d2c82167fd5ebfdca0160f3b76cc33fc125a0f8c
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-05-10 16:26:40 +0300

    list created

---
+++ src/components/List/List.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ListItem from "../ListItem/ListItem";
+import "./List.css"
+const List = (props) => {
+  const { datas } = props;
+  console.log(datas);
+  return (
+    <div className="list__root">
+      {datas.map((item, index) => {
+        return <ListItem key={item.id} item={item} />;
+      })}
+    </div>
+  );
+};
+
+export default List;
