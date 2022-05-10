13e8cfea185e27f7040bae815db742ed6bf01486
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-05-10 18:37:46 +0300

    items aligned to center

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,10 +2,16 @@ import React from "react";
 import List from "../../components/List/List";
 import { data } from "../../data/data";
 
-
 const HomePage = () => {
   return (
-    <div>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
       <h1>Address Book</h1>
       <List datas={data} />
     </div>
