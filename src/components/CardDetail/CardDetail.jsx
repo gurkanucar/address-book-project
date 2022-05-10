5f97e53a8e37b273576c7d4f0f3c911b1c37168c
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-05-10 18:36:25 +0300

    Components implemented to card detail

--- src/components/CardDetail/CardDetail.jsx
+++ src/components/CardDetail/CardDetail.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import AddressComponent from "../AddressComponent/AddressComponent";
+import BirthComponent from "../BirthComponent/BirthComponent";
 import PhoneNumbers from "../PhoneNumbers/PhoneNumbers";
 import SocialMedia from "../SocialMedia/SocialMedia";
 import "./CardDetail.css";
@@ -35,10 +36,11 @@ const CardDetail = (props) => {
 
   return (
     <div className="card-detail__root">
-        {/* <hr className="hr"></hr> */}
-       <PhoneNumbers data={props.data}/>
-      <SocialMedia data={props.data}/>
+      {/* <hr className="hr"></hr> */}
+      <PhoneNumbers data={props.data} />
+      <SocialMedia data={props.data} />
       <AddressComponent data={props.data} />
+      <BirthComponent data={props.data} />
     </div>
   );
 };
