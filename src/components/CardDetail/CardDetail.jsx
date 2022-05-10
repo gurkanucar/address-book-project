c7ad9f716775c33d31f53fb54165235e8f296e87
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-05-10 16:29:48 +0300

    Card Detail created

---
+++ src/components/CardDetail/CardDetail.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import AddressComponent from "../AddressComponent/AddressComponent";
+import PhoneNumbers from "../PhoneNumbers/PhoneNumbers";
+import SocialMedia from "../SocialMedia/SocialMedia";
+import "./CardDetail.css";
+
+const CardDetail = (props) => {
+  const {
+    id,
+    first_name,
+    last_name,
+    email,
+    gender,
+    job,
+    phone_number,
+    work_phone_number,
+    home_phone_number,
+    country,
+    city,
+    home_address,
+    work_address,
+    facebook,
+    instagram,
+    twitter,
+    linkedin,
+    github,
+    family_intimacy,
+    category,
+    created,
+    color,
+    birth_date,
+    imageUrl,
+    place_of_birth,
+  } = props.data;
+
+  return (
+    <div className="card-detail__root">
+        {/* <hr className="hr"></hr> */}
+       <PhoneNumbers data={props.data}/>
+      <SocialMedia data={props.data}/>
+      <AddressComponent data={props.data} />
+    </div>
+  );
+};
+
+export default CardDetail;
