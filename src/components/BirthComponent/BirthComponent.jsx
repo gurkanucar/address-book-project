3b84211d968c11f25284156a8b82cdbd8b8b2cf3
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-05-10 18:35:52 +0300

    Birth component created

---
+++ src/components/BirthComponent/BirthComponent.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { _calculateAge } from "../../util/ageCalculator";
+import "./BirthComponent.css";
+import { FaBirthdayCake } from "react-icons/fa";
+
+const BirthComponent = (props) => {
+  const { birth_date, place_of_birth, gender } = props.data;
+
+  return (
+    <div className="birth__item">
+      {(birth_date || place_of_birth || gender) && (
+        <div>
+          <FaBirthdayCake />
+          <span className="birth__title">Birth</span>
+        </div>
+      )}
+      <br />
+      {birth_date && (
+        <h3 className="birth__label">
+          <strong>Birth Date:</strong> {birth_date}
+        </h3>
+      )}
+      {birth_date && (
+        <h3 className="birth__label">
+          <strong>Age:</strong> {_calculateAge(new Date(birth_date))}
+        </h3>
+      )}
+      {place_of_birth && (
+        <h3 className="birth__label">
+          <strong>Place:</strong> {place_of_birth}
+        </h3>
+      )}
+      {gender && (
+        <h3 className="birth__label">
+          <strong>Gender:</strong> {gender}
+        </h3>
+      )}
+    </div>
+  );
+};
+
+export default BirthComponent;
