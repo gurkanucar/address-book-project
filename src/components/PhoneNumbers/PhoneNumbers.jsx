e678e19232d3eccdce8243a98b9a74f3845f7018
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-05-10 18:37:22 +0300

    icons added to PhoneNumber component

--- src/components/PhoneNumbers/PhoneNumbers.jsx
+++ src/components/PhoneNumbers/PhoneNumbers.jsx
@@ -1,24 +1,31 @@
 import React from "react";
 import "./PhoneNumbers.css";
+import { FaPhone } from "react-icons/fa";
 const PhoneNumbers = (props) => {
   const { home_phone_number, work_phone_number, phone_number } = props.data;
 
   return (
-    <div className="phone-numbers__item">
-      {(home_phone_number || work_phone_number) && (
-        <span className="phone-numbers__title">Phones</span>
-      )}
-      {home_phone_number && (
-        <h3 className="phone-numbers__label">
-          <strong>Home:</strong> {home_phone_number}
-        </h3>
-      )}
-      {work_phone_number && (
-        <h3 className="phone-numbers__label">
-          <strong>Work:</strong> {work_phone_number}
-        </h3>
-      )}
-    </div>
+    (home_phone_number || work_phone_number) && (
+      <div className="phone-numbers__item">
+        {
+          <div>
+            <FaPhone />
+            <span className="phone-numbers__title">Phones</span>
+          </div>
+        }
+        <br />
+        {home_phone_number && (
+          <h3 className="phone-numbers__label">
+            <strong>Home:</strong> {home_phone_number}
+          </h3>
+        )}
+        {work_phone_number && (
+          <h3 className="phone-numbers__label">
+            <strong>Work:</strong> {work_phone_number}
+          </h3>
+        )}
+      </div>
+    )
   );
 };
 
