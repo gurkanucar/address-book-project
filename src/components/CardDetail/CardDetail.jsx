a6f5caab0ef86a3c5e513ee81a332eb85f81323c
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-05-11 19:14:37 +0300

    card details view improved

--- src/components/CardDetail/CardDetail.jsx
+++ src/components/CardDetail/CardDetail.jsx
@@ -1,4 +1,10 @@
 import React from "react";
+import {
+  checkAddresses,
+  checkBirth,
+  checkPhoneNumbers,
+  checkSocialMedias,
+} from "../../util/checkDatas";
 import AddressComponent from "../AddressComponent/AddressComponent";
 import BirthComponent from "../BirthComponent/BirthComponent";
 import PhoneNumbers from "../PhoneNumbers/PhoneNumbers";
@@ -6,13 +12,15 @@ import SocialMedia from "../SocialMedia/SocialMedia";
 import "./CardDetail.css";
 
 const CardDetail = (props) => {
+  const { data } = props;
+
   return (
     <div className="card-detail__root">
       {/* <hr className="hr"></hr> */}
-      <PhoneNumbers data={props.data} />
-      <SocialMedia data={props.data} />
-      <AddressComponent data={props.data} />
-      <BirthComponent data={props.data} />
+      {checkPhoneNumbers(data) && <PhoneNumbers data={data} />}
+      {checkSocialMedias(data) && <SocialMedia data={data} />}
+      {checkAddresses(data) && <AddressComponent data={data} />}
+      {checkBirth(data) && <BirthComponent data={data} />}
     </div>
   );
 };
