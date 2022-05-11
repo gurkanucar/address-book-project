48ad50460e7251fa3fc6cd04e7b21d67ba931da4
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-05-11 13:09:32 +0300

    Input Component updated

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
-import "./Input.css"
-const Input = () => {
-  return (
-    <div>Input</div>
-  )
-}
+import React from "react";
+import "./Input.css";
+const Input = (props) => {
+  const { labelName, name, type, onChange, value, placeholder, error } = props;
 
-export default Input
+  return (
+    <div className="input-component__item">
+      <label className="input-component__label" for={name}>
+        {labelName}
+      </label>
+      <input
+        className="input-component__input "
+        type={type ? type : "text"}
+        name={name}
+        onChange={onChange}
+        values={value}
+        placeholder={placeholder}
+      />
+      <p className="input-component__error">{error ? error : " "}</p>
+    </div>
+  );
+};
+
+export default Input;
