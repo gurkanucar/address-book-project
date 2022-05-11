09111468d8cdbb0303a924907dc90605600eb163
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-05-11 14:09:53 +0300

    default value field added to input component

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,19 +1,29 @@
 import React from "react";
 import "./Input.css";
 const Input = (props) => {
-  const { labelName, name, type, onChange, value, placeholder, error } = props;
+  const {
+    labelName,
+    defaultValue,
+    name,
+    type,
+    onChange,
+    value,
+    placeholder,
+    error,
+  } = props;
 
   return (
     <div className="input-component__item">
-      <label className="input-component__label" for={name}>
+      <label className="input-component__label" htmlFor={name}>
         {labelName}
       </label>
       <input
-        className="input-component__input "
+        className="input-component__input"
         type={type ? type : "text"}
         name={name}
         onChange={onChange}
         values={value}
+        defaultValue={defaultValue}
         placeholder={placeholder}
       />
       <p className="input-component__error">{error ? error : " "}</p>
