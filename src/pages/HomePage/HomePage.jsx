8dee2ab25dc659bc19ec6837640ce06e7a98d8b6
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-05-12 02:02:25 +0300

    search component implemented to home page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -5,10 +5,18 @@ import List from "../../components/List/List";
 import { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import FabButton from "../../components/FabButton/FabButton";
+import SearchComponent from "../../components/SearchComponent/SearchComponent";
+import { searchFilter, searchFilterRestrict } from "../../util/filterData";
 
 const HomePage = () => {
   const dispatch = useDispatch();
   const addressList = useSelector((state) => state.addresses.value);
+  const [searchedText, setSearchedText] = useState("");
+  const [category, setCategory] = useState("");
+  const [gender, setGender] = useState("");
+  const [country, setCountry] = useState("");
+
+  const [including, setIncluding] = useState(false);
 
   return (
     <div
@@ -20,7 +28,29 @@ const HomePage = () => {
       }}
     >
       <h1>Address Book</h1>
-      <List datas={addressList} />
+      <SearchComponent
+        searchedText={searchedText}
+        setSearchedText={setSearchedText}
+        country={country}
+        setCountry={setCountry}
+        category={category}
+        setCategory={setCategory}
+        gender={gender}
+        setGender={setGender}
+      />
+      <List
+        datas={
+          including == true
+            ? searchFilter(addressList, searchedText, category, country, gender)
+            : searchFilterRestrict(
+                addressList,
+                searchedText,
+                category,
+                country,
+                gender
+              )
+        }
+      />
       <FabButton />
     </div>
   );
