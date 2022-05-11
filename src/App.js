cf6c5941c0ff88adea035d05dd48fcdbbb82469b
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-05-11 12:18:52 +0300

    Routers implemented

--- src/App.js
+++ src/App.js
@@ -2,11 +2,27 @@ import logo from "./logo.svg";
 import "./App.css";
 import HomePage from "./pages/HomePage/HomePage";
 
+//router
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import CreateOrEditPage from "./pages/CreateOrEditPage/CreateOrEditPage";
+import DetailPage from "./pages/DetailPage/DetailPage";
+
 function App() {
   return (
-    <div>
-      <HomePage />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="addresses/:id" element={<DetailPage />} />
+        <Route
+          path="addresses/edit/:id"
+          element={<CreateOrEditPage isNew={false} />}
+        />
+        <Route
+          path="addresses/new"
+          element={<CreateOrEditPage isNew={true} />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
