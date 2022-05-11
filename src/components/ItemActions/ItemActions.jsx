761c345093b96dff4f9cb92014612a06c4de126f
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-05-11 22:14:18 +0300

    Item Actions component created

---
+++ src/components/ItemActions/ItemActions.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./ItemActions.css";
+
+import { MdEdit, MdDelete } from "react-icons/md";
+
+const ItemActions = (props) => {
+  const { id, setId, setIsAlertModalOpen } = props;
+  let navigate = useNavigate();
+  return (
+    <div>
+      <div className="item__actions">
+        <MdEdit
+          onClick={() => {
+            navigate("addresses/edit/" + id);
+          }}
+          size={25}
+        />
+        <MdDelete
+          onClick={() => {
+            setIsAlertModalOpen(true);
+            setId(id);
+          }}
+          size={25}
+          color={"#f2493d"}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ItemActions;
