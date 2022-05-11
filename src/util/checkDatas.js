7518a43d43202e8259f39269434fa6452ba274b2
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-05-11 19:13:52 +0300

    data checker for detail items implemented

---
+++ src/util/checkDatas.js
@@ -0,0 +1,21 @@
+export const checkSocialMedias = (data) => {
+  return (
+    data.instagram ||
+    data.facebook ||
+    data.linkedin ||
+    data.twitter ||
+    data.github
+  );
+};
+
+export const checkPhoneNumbers = (data) => {
+  return data.home_phone_number || data.work_phone_number;
+};
+
+export const checkAddresses = (data) => {
+  return data.home_address || data.work_address || data.city || data.country;
+};
+
+export const checkBirth = (data) => {
+  return data.instagram || data.birth_date, data.place_of_birth, data.gender;
+};
