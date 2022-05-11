bb8237b0b48fa0ee2a1a9041a5d540550273cb18
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-05-12 02:02:12 +0300

    search component created

---
+++ src/components/SearchComponent/SearchComponent.jsx
@@ -0,0 +1,92 @@
+import React, { useEffect, useState } from "react";
+import "./SearchComponent.css";
+import Input from "../Input/Input";
+import { countries } from "../../data/Countries";
+import ShowMoreComponent from "../ShowMoreComponent/ShowMoreComponent";
+const SearchComponent = (props) => {
+  const {
+    searchedText,
+    setSearchedText,
+    category,
+    setCategory,
+    country,
+    setCountry,
+    gender,
+    setGender,
+  } = props;
+
+  const genderOptions = [
+    { value: "", text: "" },
+    { value: "male", text: "male" },
+    { value: "female", text: "female" },
+    { value: "male", text: "male" },
+  ];
+  const categoryOptions = [
+    { value: "", text: "" },
+    { value: "family", text: "family" },
+    { value: "friend", text: "friend" },
+    { value: "job", text: "job" },
+    { value: "sport", text: "sport" },
+    { value: "school", text: "school" },
+    { value: "other", text: "other" },
+  ];
+
+  const [expand, setExpand] = useState(false);
+
+  return (
+    <div className="search-component__div">
+      <input
+        className="input-component__input"
+        value={searchedText}
+        onChange={(e) => setSearchedText(e.target.value.toLowerCase())}
+        placeholder="words.."
+      />
+
+      <select
+        onChange={(e) => setCountry(e.target.value.toLowerCase())}
+        value={country}
+        className="input-component__input"
+      >
+        {countries.map((x, index) => {
+          return (
+            <option key={index} value={x.value}>
+              {x.text}
+            </option>
+          );
+        })}
+      </select>
+
+      <select
+        onChange={(e) => setGender(e.target.value.toLowerCase())}
+        value={gender}
+        className="input-component__input"
+      >
+        {genderOptions.map((x, index) => {
+          return (
+            <option key={index} value={x.value}>
+              {x.text}
+            </option>
+          );
+        })}
+      </select>
+
+      <select
+        onChange={(e) => setGender(e.target.value.toLowerCase())}
+        value={gender}
+        className="input-component__input"
+      >
+        {categoryOptions.map((x, index) => {
+          return (
+            <option key={index} value={x.value}>
+              {x.text}
+            </option>
+          );
+        })}
+      </select>
+
+      <ShowMoreComponent setExpand={setExpand} expand={expand} />
+    </div>
+  );
+};
+
+export default SearchComponent;
