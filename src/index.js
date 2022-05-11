324e5c385801e82366c78419a6ab92d84a1525e8
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-05-11 12:20:00 +0300

    redux implemented to index js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+//redux
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+
+import addressReducer from "./reducer/Addresses";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const store = configureStore({
+  reducer: {
+    addresses: addressReducer,
+  },
+});
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
