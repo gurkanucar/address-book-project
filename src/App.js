ab60d65a20b4ec38240f84856289a1537108ead7
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-05-12 21:08:33 +0300

    PrivateRoutes and 404 Page implemented

--- src/App.js
+++ src/App.js
@@ -3,26 +3,40 @@ import "./App.css";
 import HomePage from "./pages/HomePage/HomePage";
 
 //router
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+  Outlet,
+  useLocation,
+} from "react-router-dom";
 import CreateOrEditPage from "./pages/CreateOrEditPage/CreateOrEditPage";
 import DetailPage from "./pages/DetailPage/DetailPage";
 import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
+import LoginPage from "./pages/LoginPage/LoginPage";
+
+import { PrivateRoute } from "./components/PrivateRoute/PrivateRoute";
 
 function App() {
+  //get auth data
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="addresses/:id" element={<DetailPage />} />
-        <Route
-          path="addresses/edit/:id"
-          element={<CreateOrEditPage isNew={false} />}
-        />
-        <Route
-          path="addresses/new"
-          element={<CreateOrEditPage isNew={true} />}
-        />
-        <Route path="404" element={<NotFoundPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+        <Route path="/" element={<PrivateRoute />}>
+          <Route path="/home" element={<HomePage />} />
+          <Route
+            path="/addresses/edit/:id"
+            element={<CreateOrEditPage isNew={false} />}
+          />
+          <Route
+            path="/addresses/new"
+            element={<CreateOrEditPage isNew={true} />}
+          />
+        </Route>
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
   );
