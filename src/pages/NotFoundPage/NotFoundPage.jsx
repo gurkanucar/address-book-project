d98c974841ad08039068d2e4ce7fa240077c6db4
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-05-14 01:00:52 +0300

    unused import removed

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { URL_PREFIX } from "../../constants";
 import "./NotFoundPage.css";
 
 const NotFoundPage = () => {
